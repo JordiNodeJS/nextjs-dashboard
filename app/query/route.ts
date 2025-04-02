45afb4bfedbe9423b1af8a4925198f574e5c2860
JordiNodejs <jordi.nodejs@gmail.com>
2025-04-02 20:15:58 +0200

    Refactor join handling in API to use a mapping object for cleaner code

--- app/query/route.ts
+++ app/query/route.ts
@@ -38,27 +38,29 @@ async function getFullJoin() {
   return data;
 }
 
+const joinQueries = {
+  inner: getInnerJoin,
+  left: getLeftJoin,
+  right: getRightJoin,
+  full: getFullJoin,
+} as const;
+
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
-  const joinType = searchParams.get("join");
+  const joinType = searchParams.get("join") as keyof typeof joinQueries;
 
   try {
-    switch (joinType) {
-      case "inner":
-        return Response.json(await getInnerJoin());
-      case "left":
-        return Response.json(await getLeftJoin());
-      case "right":
-        return Response.json(await getRightJoin());
-      case "full":
-        return Response.json(await getFullJoin());
-      default:
-        return Response.json({
-          message:
-            "Specify join type in query parameter: ?join=inner|left|right|full",
-          example: "/query?join=inner",
-        });
+    const queryFn = joinQueries[joinType];
+
+    if (queryFn) {
+      return Response.json(await queryFn());
     }
+
+    return Response.json({
+      message:
+        "Specify join type in query parameter: ?join=inner|left|right|full",
+      example: "/query?join=inner",
+    });
   } catch (error) {
     return Response.json({ error }, { status: 500 });
   }
