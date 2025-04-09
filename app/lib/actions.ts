247b182975b0e25dac59aa3374933f09503ce320
JordiNodejs <jordi.nodejs@gmail.com>
2025-04-10 00:25:16 +0200

    feat(invoices): add server action to create and validate invoices
    Implement server action `createInvoice` to handle form data, validate it using Zod, and insert the validated data into the PostgreSQL database. Additionally, revalidate the invoices page to reflect the new invoice. This ensures data integrity and improves user experience by keeping the UI up-to-date.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,11 +1,41 @@
 "use server";
+/* 
+El enrutamiento es automático - La función createInvoice definida con "use server" 
+se convierte automáticamente en un endpoint interno
+*/
+import { z } from "zod";
+import postgres from "postgres";
+import { revalidatePath } from 'next/cache';
+
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+
+const FormSchema = z.object({
+   id: z.string(),
+   customerId: z.string(),
+   amount: z.coerce.number(),
+   status: z.enum(['pending', 'paid'], {
+      required_error: 'Status is required.'
+   }),
+ date: z.string().datetime()
+})
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
-    const rawFormData = {
-      customerId: formData.get('customerId'),
-      amount: formData.get('amount'),
-      status: formData.get('status'),
-    };
-    // Test it out:
-    console.log(rawFormData);
-  }
+    const data = Object.fromEntries(formData.entries());
+    const { customerId, amount, status } = CreateInvoice.parse(data); ;
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
+    const invoice = { customerId, amount: amountInCents, status, date };
+
+    // await sql`
+    // INSERT INTO invoices ${sql(invoice)}`;
+    await sql`
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+`;	
+    
+    // Revalidate the invoices page to show the new invoice
+    revalidatePath('/dashboard/invoices');
+}
