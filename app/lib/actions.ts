78de18140baec5c47af15d0948571567b94b4354
JordiNodejs <jordi.nodejs@gmail.com>
2025-04-10 16:46:43 +0200

    feat(invoices): add error handling and notFound for missing invoices
    This commit introduces error handling for database operations in the invoices module, including create, update, and delete actions. Additionally, it adds a `notFound` redirect for missing invoices in the edit page. A new error component is also added to handle and display errors gracefully.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -12,63 +12,83 @@ import { redirect } from 'next/navigation';
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 const FormSchema = z.object({
-   id: z.string(),
-   customerId: z.string(),
-   amount: z.coerce.number(),
-   status: z.enum(['pending', 'paid'], {
-      required_error: 'Status is required.'
-   }),
- date: z.string().datetime()
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid'], {
+    required_error: 'Status is required.'
+  }),
+  date: z.string().datetime()
 })
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
-    const data = Object.fromEntries(formData.entries());
-    const { customerId, amount, status } = CreateInvoice.parse(data); ;
-    const amountInCents = amount * 100;
-    const date = new Date().toISOString().split('T')[0];
-    const invoice = { customerId, amount: amountInCents, status, date };
+  const data = Object.fromEntries(formData.entries());
+  const { customerId, amount, status } = CreateInvoice.parse(data);;
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+  const invoice = { customerId, amount: amountInCents, status, date };
 
-    // await sql`
-    // INSERT INTO invoices ${sql(invoice)}`;
+  // await sql`
+  // INSERT INTO invoices ${sql(invoice)}`;
+
+  try {
     await sql`
-        INSERT INTO invoices (customer_id, amount, status, date)
-        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-`;	
-    
-    // Revalidate the invoices page to show the new invoice
-    revalidatePath('/dashboard/invoices');
-    redirect('/dashboard/invoices');
+            INSERT INTO invoices (customer_id, amount, status, date)
+            VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    }
+    `;
+  } catch (error) {
+    return {
+      message: 'Database Error: Failed to Create Invoice.'
+    };
+  }
+
+  // Revalidate the invoices page to show the new invoice
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
 }
 
 // Use Zod to update the expected types
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function updateInvoice(id: string, formData: FormData) {
-    const { customerId, amount, status } = UpdateInvoice.parse({
-      customerId: formData.get('customerId'),
-      amount: formData.get('amount'),
-      status: formData.get('status'),
-    });
-   
-    const amountInCents = amount * 100;
-   
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  const amountInCents = amount * 100;
+
+  try {
     await sql`
-      UPDATE invoices
-      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+      `;
+  } catch (error) {
+    return { message: 'Database Error: Failed to Update Invoice.' };
+  }
+
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+  // throw new Error('Failed to Delete Invoice');
+
+  // Delete invoices associated with the invoice
+  try {
+    await sql`
+      DELETE FROM invoices
       WHERE id = ${id}
     `;
-   
-    revalidatePath('/dashboard/invoices');
-    redirect('/dashboard/invoices');
+  } catch (error) {
+    return { message: 'Database Error: Failed to Delete Invoice.' };
   }
 
-  export async function deleteInvoice(id: string) {
-    // throw new Error('Failed to Delete Invoice');
-
-    // Delete invoices associated with the invoice
-    await sql`DELETE FROM invoices WHERE id = ${id}`;
-
-    revalidatePath('/dashboard/invoices');
-  }
+  revalidatePath('/dashboard/invoices');
+}
